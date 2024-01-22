bf78a4a22c9fa36d8c66976a89b4721e3f85d2ee
jhonzasd <jhonjairotejada@gmail.com>
2024-01-21 21:18:24 -0500

    Primera build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+// Configuración de Astro con Tailwind CSS
 export default defineConfig({
-  integrations: [tailwind()]
+  // Configuración de Astro puede ir aquí
+
+  // Integración de Tailwind CSS
+  integrations: [tailwind()],
+
+  // Configuración adicional de Tailwind si es necesario
+  tailwind: {
+    // Configuración de Tailwind
+  },
+
+  // Configuración de la ruta base para GitHub Pages
+  site: "https://jhonzasd.github.io", // Reemplaza "nombre-de-repositorio" con el nombre real de tu repositorio en GitHub
+
+  // Puedes agregar más configuraciones según sea necesario
 });
+
+// Script de implementación en GitHub Pages
+// Agrega este script en el archivo "package.json"
+// Luego, puedes ejecutar "npm run deploy" para implementar en GitHub Pages
+
